f448bcc12f7d88faaae1e52b7720851d616ec46c
Kenei Menning <menningkenei@gmail.com>
2025-08-05 10:43:31 -0500

    chore: update database models index to export new models

--- server/database/models/index.ts
+++ server/database/models/index.ts
@@ -3,6 +3,8 @@ export { Song, type ISong, type Difficulty } from "./Song";
 export { User, type IUser, type MusicalKey } from "./User";
 export { Arrangement, type IArrangement } from "./Arrangement";
 export { Setlist, type ISetlist, type ISetlistItem } from "./Setlist";
+export { Verse, type IVerse, type VerseStatus } from "./Verse";
+export { Review, type IReview } from "./Review";
 
 // Re-export mongoose types for convenience
 export { Types } from "mongoose";
